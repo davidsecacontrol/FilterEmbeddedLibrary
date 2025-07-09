8559967451831123b53049a996855fd3a44a59a0
dsecad22 <davidsecacontrol@gmail.com>
2025-07-09 14:29:47 +0200

    QoL & idiomatic improvements
    Proper indentation.
    Improved names to be idiomatic
    Added different coeff initialization options

--- Filter.h
+++ Filter.h
@@ -9,22 +9,28 @@
 **
 ** -------------------------------------------------------------
 **
-**     Implements digital filters of the type: 
+**     Implements digital filters of the type:
 **
-**       y[n] =  a[0]*x[n] + a[1]*x[n-1] + ... + a[N-1]*x[n-(N-1)] 
-**                  + b[1]*y[n-1] + b[2]*y[n-2] + ... + b[N-1]*y[n-1-(N-1)]
+**       y[n] =  a[0]*x[n] + a[1]*x[n-1] + ... + a[N-1]*x[n-(N-1)]
+**                  + b[1]*y[n-1] + b[2]*y[n-2] + ... + b[N-1]*y[n-(N-1)]
 **
-**      or: 
+**      with SetCoefficients(a,b) 
+**
+**      or:
 **
 **          y(z)       a[0] + a[1]*z + ... + a[N-1]*z^(N-1)
 **        --------- = ------------------------------------------
-**          x(z)       b[0] + b[1]*s + ... + b[N-1]*z[N-1]
+**          x(z)       b[0] + b[1]*z + ... + b[N-1]*z^(N-1)
+**
+**     with SetCoefficientsFromZTransform(a,b)
+**
+**
 **
 **     Where:
 **        numerator[n] <-> a[n]
 **        denominator[n] <-> b[n]
-** 
-**           
+**
+**
 **
 *******************************************************
 Future Updates:
@@ -41,52 +47,60 @@ Future Updates:
 #ifndef FILTER_H
 #define FILTER_H
 
-
 #include <cassert>
 
 #include "Filter_utils.h"
-
 
 template <typename Type, unsigned int N>
 class Filter
 {
 
 public:
-  float numerator[N] = {};    // a[]        where a[0] <-> x[n]
-  float denominator[N] = {};  // b[]      where b[0] = 1 <-> y[n]
+  float numerator[N] = {};   // a[]        where a[0] <-> x[n]
+  float denominator[N] = {}; // b[]      where b[0] = 1 <-> y[n]
 
-  // Normalizes b[n] such that b[0] = 1
-  void setCoeffs(float const *const numerator, float const *const denominator);
+  void SetCoefficients(float const *const numerator, float const *const denominator);
+  // Computes filter coefficients from z transform
+  void SetCoefficientsFromZTransform(float const *const numerator, float const *const denominator);
 
   // Compute next filter respose
   Type Update(const Type new_value);
 
 private:
-  Type input[N] = {};         // x[n]
-  Type output[N] = {};        // y[n]
-
-  
-  
+  Type input[N] = {};  // x[n]
+  Type output[N] = {}; // y[n]
 };
 
 // Filter definitions ----------------------------------------------------------------------
 
 template <typename Type, unsigned int N>
-void Filter<Type, N>::setCoeffs(float const *const numerator, float const *const denominator)
+void Filter<Type, N>::SetCoefficients(float const *const numerator, float const *const denominator)
 {
-  // If b[0] = 0, the filter coefficients are incorrect.
-  assert(denominator[0] != 0);
 
   // Store the inputs
   CopyArray(this->numerator, numerator, N);
   CopyArray(this->denominator, denominator, N);
+}
 
-  // Solve for y(z) by normalizing filter coefficients (required b[0] = 1)
-  DivideArrayElements(this->numerator  ,N, this->denominator[0]);
-  DivideArrayElements(this->denominator,N, this->denominator[0]);
-  
+template <typename Type, unsigned int N>
+void Filter<Type, N>::SetCoefficientsFromZTransform(float const *const numerator, float const *const denominator)
+{
+  SetCoefficients(numerator, denominator);
 
+  // If b[0] = 0, the filter coefficients are incorrect.
+  assert(denominator[0] != 0);
 
+  // Solve for y(z) by
+
+  // normalizing filter coefficients (required b[0] = 1)
+  DivideArrayElements(this->numerator, N, this->denominator[0]);
+  DivideArrayElements(this->denominator, N, this->denominator[0]);
+
+  // and negating b[2...N]
+  for (unsigned int i = 1; i < N; i++)
+  {
+    this->denominator[i] *= -1.0f;
+  }
 }
 
 template <typename Type, unsigned int N>
--- main.cpp
+++ main.cpp
@@ -3,22 +3,23 @@
 
 constexpr unsigned int FILTER_SIZE = 5;
 
-Filter<float,FILTER_SIZE> moving_average;
+Filter<float, FILTER_SIZE> moving_average;
 
-int main(){
-    float num[] = {0.2,0.2,0.2,0.2,0.2};
-    float den[] = {1,0,0,0,0};
-    moving_average.setCoeffs(num,den);
+int main()
+{
+    float num[] = {0.2, 0.2, 0.2, 0.2, 0.2};
+    float den[] = {1, 0, 0, 0, 0};
+    moving_average.SetCoefficients(num, den);
 
-    
     std::cout << "Selected filter is moving average(5)" << std::endl;
 
     int i = 0;
-    while(true){
+    while (true)
+    {
         float new_sample;
         std::cout << "Input " << i << ":";
         std::cin >> new_sample;
-        
+
         std::cout << "Output " << i << ":" << moving_average.Update(new_sample) << std::endl;
         ++i;
     }
