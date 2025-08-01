6b7c575565459fd0a70da11e5e6af6ab74010520
dsecad22 <davidsecacontrol@gmail.com>
2025-08-02 00:16:30 +0200

    Testing: Added Skeleton for class testing
    Now the tests must be implemented as defined. Moreover, the filter class must change its values from private to protected, so they can be tested.

--- tests/Filter_testing.cpp
+++ tests/Filter_testing.cpp
@@ -1,8 +1,35 @@
 #include <gtest/gtest.h>
 
-// Tests must 
-TEST(HelloTest,BasicAssertions) {
-    EXPECT_STRNE("hello","world");
+#include "Filter.hpp"
 
+/* Test suite 1: PublicAPITests Collects tests to ensure the API works as expected. If it breaks, we'll find out
+**
+** Last updated: 01/08/2025
+*/
+
+// Create a FilterTestHelper class that derives from FilterTest to get access to protected members
+
+TEST(PublicAPITests,InitializationTest_SetCoefficients) {
+    // test values are copied properly
+    // Test states are set properly(0...0,0...0)
+    EXPECT_EQ(7*6,42);
+}
+
+
+TEST(PublicAPITests,InitializationTest_SetCoefficientsFromZTransform) {
+    // Test values are computed proeprly
+    // Test states are set properly (0...0,0...0)
+    EXPECT_EQ(7*6,42);
+}
+
+TEST(PublicAPITests,ResultTest_Update) {
+    // Ensure return value works as an expected filter: 
+    //fx select a couple filters from MATLAB and test results match
+    EXPECT_EQ(7*6,42);
+}
+
+TEST(PublicAPITests,InitialziationTest_SetState) {
+    // Test values are computed properly
+    // Test states are set properly (0...0,0...0)
     EXPECT_EQ(7*6,42);
 }
