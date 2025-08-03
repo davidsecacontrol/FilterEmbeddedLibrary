2960ed65224acda36510cd04af8d77870902c854
dsecad22 <davidsecacontrol@gmail.com>
2025-08-03 13:22:43 +0200

    Testing: Added update() test

--- tests/Filter_testing.cpp
+++ tests/Filter_testing.cpp
@@ -112,13 +112,30 @@ TEST(PublicAPITests,SetCoefficientsFromZTransform_HandlesDivideByZero) {
 }
 
 /**
- * @brief Tests Update() with a ____ filter - TO BE IMPLEMENTED
+ * @brief Tests Update() with a butterworth filter with precision 0.002f
  * 
  */
-TEST(PublicAPITests,ResultTest_Update) {
-    // Ensure return value works as an expected filter: 
-    //fx select a couple filters from MATLAB and test results match
-    EXPECT_EQ(7*6,42);
+TEST(PublicAPITests,Update_TestFilterOutput) {
+    constexpr unsigned int N = 6;
+    float numerator[N] = {0.0013f,0.0064f,0.0128f,0.0128f,0.0064f,0.0013f};
+    float denominator[N] = {1.0000f,-2.9754f,3.8060f,-2.5453f,0.8811f,-0.1254f};
+
+    FilterTestHelper<float,N> butter_filter; // 5th order 0.2 rad/s normalized cutoff
+    butter_filter.SetCoefficientsFromZTransform(numerator,denominator);
+
+    // Compute output data:
+    constexpr unsigned int n_samples = 10;
+    float input[n_samples] = {9.8196f,-5.2901f,-5.0688f,10.4616f,1.0000f,-8.5595f,6.6869f,6.4656f,-9.2015f,-0.0000f};
+    float output[n_samples] = {0};
+
+    for(unsigned int i = 0; i < n_samples; i++){
+        output[i] = butter_filter.Update(input[i]);
+    }
+    //Compare with expected output:
+    float output_expected[n_samples] = {0.0126f,0.0937f,0.3163f,0.6556f,0.9728f,1.1665f,1.2315f,1.1774f,1.0356f,0.8850f};
+    for(unsigned int i = 0; i < n_samples; i++){
+        ASSERT_NEAR(output[i],output_expected[i],0.003f);
+    }
 }
 
 /**
